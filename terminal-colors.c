52ff3e0d2bf86cabb47a860c6f5e64e7e94b542d
Simon Gomizelj <simongmzlj@gmail.com>
2014-12-18 00:27:54 -0500

    Check for 'disable' file and scan directory

--- terminal-colors.c
+++ terminal-colors.c
@@ -1,13 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
 #include <err.h>
+#include <dirent.h>
 
 int main(int argc, char *argv[])
 {
+
     if (access("/etc/terminal-colors.d/", R_OK) < 0)
         err(1, "couldn't access terminal-colors.d");
 
-    /* const char *program = argv[1]; */
-    /* if (argc != 2) */
-    /*     return -1; */
+    int test = access("/etc/terminal-colors.d/disable", R_OK);
+    if (test == 0) {
+        printf("TERMINAL COLORS DISABLED!\n");
+        return 0;
+    } else if (test < 0 && errno != ENOENT) {
+        err(1, "couldn't access terminal-colors.d/disable");
+    }
+
+    int dirfd;
+    DIR *dirp;
+
+    dirfd = open("/etc/terminal-colors.d/", O_RDONLY | O_DIRECTORY);
+    dirp = fdopendir(dirfd);
+    if (!dirp)
+        err(1, "fdopendir failed");
+
+    const struct dirent *dp;
+    while ((dp = readdir(dirp))) {
+        if (dp->d_type != DT_REG && dp->d_type != DT_UNKNOWN)
+            continue;
+
+        printf("found: %s\n", dp->d_name);
+    }
 }
