28f2bbaff4e48afaa8e1f74044fbada04fc72dad
Simon Gomizelj <simongmzlj@gmail.com>
2014-12-18 00:46:35 -0500

    Terrible memory managment, must fix

--- terminal-colors.c
+++ terminal-colors.c
@@ -1,10 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
 #include <err.h>
 #include <dirent.h>
+
+static void parse_support(const char *name)
+{
+    char *dup = strdup(name);
+    size_t len = strlen(name);
+
+    size_t at = strcspn(name, "@");
+    size_t dot = strcspn(name, ".");
+
+    dup[at] = 0;
+    dup[dot] = 0;
+
+    // FIXME: memory overflow
+    printf("name: %s\n", dup);
+    printf("at:   %s\n", &dup[at + 1]);
+    printf("dot:  %s\n", &dup[dot + 1]);
+    printf("\n");
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,6 +31,7 @@ int main(int argc, char *argv[])
     if (access("/etc/terminal-colors.d/", R_OK) < 0)
         err(1, "couldn't access terminal-colors.d");
 
+    // TODO: disable can be overridden by an explicit enable
     int test = access("/etc/terminal-colors.d/disable", R_OK);
     if (test == 0) {
         printf("TERMINAL COLORS DISABLED!\n");
@@ -24,6 +44,9 @@ int main(int argc, char *argv[])
     DIR *dirp;
 
     dirfd = open("/etc/terminal-colors.d/", O_RDONLY | O_DIRECTORY);
+    if (dirfd < 0)
+        err(1, "couldn't access terminal-colors.d");
+
     dirp = fdopendir(dirfd);
     if (!dirp)
         err(1, "fdopendir failed");
@@ -33,6 +56,6 @@ int main(int argc, char *argv[])
         if (dp->d_type != DT_REG && dp->d_type != DT_UNKNOWN)
             continue;
 
-        printf("found: %s\n", dp->d_name);
+        parse_support(dp->d_name);
     }
 }
